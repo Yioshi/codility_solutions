702d3227568479fb77de1a08de544dbfb72d6996
Yioshi <martynf88@gmail.com>
2021-11-04 11:14:19 +0200

    added solution and tests

--- lesson_1/ruby/BinaryGap/test.rb
+++ lesson_1/ruby/BinaryGap/test.rb
@@ -1,1 +1,17 @@
-Tests 
+require './lessons/lesson_1/task_1/ruby/solution.rb'
+
+test_cases = {
+  '1041' => 5,
+  '15' => 0,
+  '32' => 0,
+  '20' => 1
+}
+
+
+test_cases.each do |key, val|
+  if solution(key.to_i) == val
+    puts "."
+  else
+    puts "Test failed because output was expected to be #{val}"
+  end
+end
